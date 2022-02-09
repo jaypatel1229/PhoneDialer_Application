58bd4acfe9a5cfd3974365e34058051ba1b5eaf9
jay.patel <jay.patel@1R-80>
2022-02-09 16:22:46 +0530

    Add project files.

---
+++ PhoneDiler_App/MainActivity.cs
@@ -0,0 +1,55 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+using Xamarin.Essentials;
+
+namespace PhoneDiler_App
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private Button button;
+        private EditText editText;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            button = FindViewById<Button>(Resource.Id.btn1);
+            editText = FindViewById<EditText>(Resource.Id.et1);
+
+
+            button.Click += Button_Click;
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+
+           EditText editText = FindViewById<EditText>(Resource.Id.et1);
+            
+            string number = editText.Text;
+            PhoneDialer.Open(number);  
+            //throw new NotImplementedException();
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public class PhoneDialerTest
+        {
+            public void PlacePhoneCall(string number)
+            {
+
+            }
+        }
+    }
+}
